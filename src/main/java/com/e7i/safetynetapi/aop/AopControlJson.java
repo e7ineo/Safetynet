1f79036607148b653cadf657bb494b8d3ee7984c
Eli Neo <e7ineo@outlook.com>
2023-04-30 19:40:11 +0200

    Cleaning code

--- src/main/java/com/e7i/safetynetapi/aop/AopControlJson.java
+++ src/main/java/com/e7i/safetynetapi/aop/AopControlJson.java
@@ -1,9 +1,9 @@
 package com.e7i.safetynetapi.aop;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+//import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
