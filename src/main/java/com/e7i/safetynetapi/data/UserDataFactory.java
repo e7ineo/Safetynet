544a05cadea660d70acf861849672a1c582cdeb9
Eli Neo <e7ineo@outlook.com>
2023-05-11 14:59:33 +0200

    update userdata person

--- src/main/java/com/e7i/safetynetapi/data/UserDataFactory.java
+++ src/main/java/com/e7i/safetynetapi/data/UserDataFactory.java
@@ -34,4 +34,79 @@ public class UserDataFactory {
 			}
 		}
 	}
+	
+	public static void updateUserDataPerson(Person person) {
+		boolean medicalRecordExist = false;
+		boolean firestationExist = false;
+		MedicalRecord mrRecord = new MedicalRecord();
+		Firestation fRecord = new Firestation();
+		
+		for(MedicalRecord mr : MedicalRecordDao.getMedicalRecordDao()) {
+			if(person.getFirstName().equalsIgnoreCase(mr.getFirstName()) && person.getLastName().equalsIgnoreCase(mr.getLastName())) {
+				mrRecord = mr;
+				medicalRecordExist = true;
+			}
+		}
+		
+		for(Firestation f : FirestationDao.getFirestationDao()) {
+			if(person.getAddress().equalsIgnoreCase(f.getAddress())) {
+				fRecord = f;
+				firestationExist = true;
+			}
+		}
+		
+		if(medicalRecordExist && firestationExist) {
+			UserData userData = new UserData(person, mrRecord, fRecord );
+			usersData.add(userData);
+		}
+		
+		if(medicalRecordExist && !firestationExist) {
+			UserData userData = new UserData(person, mrRecord);
+			usersData.add(userData);
+		}
+		
+		if(!medicalRecordExist && firestationExist) {
+			UserData userData = new UserData(person, fRecord );	
+			usersData.add(userData);
+		}
+		
+		if(!medicalRecordExist && !firestationExist) {
+			UserData userData = new UserData(person);
+			usersData.add(userData);
+		}		
+	}
+
+	public static void updateUserDataEditPerson(Person person) {
+		
+		for(UserData ud : usersData) {
+			if(person.getFirstName().equalsIgnoreCase(ud.getFirstName()) && person.getLastName().equalsIgnoreCase(ud.getLastName())) {
+				ud.setAddress(person.getAddress());
+				ud.setPostalCode(person.getPostalCode());
+				ud.setCity(person.getCity());
+				ud.setPhoneNumber(person.getPhoneNumber());
+				ud.setEmail(person.getEmail());	
+				for(Firestation f : FirestationDao.getFirestationDao()) {
+					if(person.getAddress().equalsIgnoreCase(f.getAddress())) {
+						ud.setFirestationNumber(f.getStationNumber());
+					}
+				}
+			}
+		}
+		
+		for(MedicalRecord mr : MedicalRecordDao.getMedicalRecordDao()) {
+			if(person.getFirstName().equalsIgnoreCase(mr.getFirstName()) && person.getLastName().equalsIgnoreCase(mr.getLastName())) {
+				mr.setFirstName(person.getFirstName());
+				mr.setLastName(person.getLastName());
+			}
+		}		
+	} 
+	
+	public static void updateUserDataDeletePerson(String firstName, String lastName) {
+		
+		for(UserData ud : usersData) {
+			if(firstName.equalsIgnoreCase(ud.getFirstName()) && lastName.equalsIgnoreCase(ud.getLastName())) {
+				usersData.remove(ud);
+			}
+		}
+	} 
 }
